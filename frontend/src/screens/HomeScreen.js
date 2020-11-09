6bd9309e75c2d3553c0acf160c2e00320f11b83e
bogdanmatei <matei90bogdan@yahoo.com>
2020-11-09 15:08:57 +0200

    Fetching articles from backend, nodemon & concurrently

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Article from '../components/Article';
-import articles from '../articles';
 import { Row, Col } from 'react-bootstrap';
 
 const HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const { data } = await axios.get('/api/articles');
+      setArticles(data);
+    };
+    fetchArticles();
+  }, []);
+
   return (
     <>
       <h1>Recent Articles</h1>
       <Row className='justify-content-center'>
-        {articles.map((article) => (
-          <Col key={article._id} sm={12} md={8} lg={8} xl={8}>
-            <Article article={article} />
-          </Col>
-        ))}
+        {articles &&
+          articles.map((article) => (
+            <Col key={article._id} sm={12} md={8} lg={8} xl={8}>
+              <Article article={article} />
+            </Col>
+          ))}
       </Row>
     </>
   );
