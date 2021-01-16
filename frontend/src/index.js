20949c4632589274f58cd1424abea472d646e777
bogdanmatei <matei90bogdan@yahoo.com>
2021-01-16 13:35:13 +0200

    Sass and style change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(
